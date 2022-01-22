64fdec4eeaceb6c91cc43a0c6e3a58ddff2f5eb6
dlancaster76 <cavanagh-lancaster_D@bisspuxi.com>
2022-01-23 00:56:12 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,20 +2,35 @@
 from tkinter import *       
 import pytesseract as tess
 import os
+import subprocess
 from PIL import Image, ImageTk
+from tkinter import Tk 
+from tkinter.filedialog import askopenfilename
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+def get_images(path):
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+        print(filename)
+  
+
+
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+print(filename)
+  
+
  
 # Creating a tkinter window
 root = Tk()           
-root.geometry('800x650')    
+root.geometry('800x600')    
  
 
 #confirm/photo buttons 
 btn1 = Button(root, text = 'Confirm')
-btn1.place(x = 325, y = 600)
+btn1.place(x = 325, y = 550)
 
-btn2 = Button(root, text = 'Import Photo')
-btn2.place(x = 390, y = 600)
+btn2 = Button(root, text = 'Import Photo', command = get_images)
+btn2.place(x = 390, y = 550)
  
 #image to text test
 image = Image.open('text1.png')
@@ -28,6 +43,8 @@ image = image.resize((550, 350), Image.ANTIALIAS)
 image1 = ImageTk.PhotoImage(image)
 label1 = Label(root, image = image1)
 label1.place(x = 125, y = 100)
+
+
 
 
 
